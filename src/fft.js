02a1782e812ca6054d954bb6682c8c30b8edda80
Devon Govett <devongovett@gmail.com>
2012-03-20 00:00:30 +0100

    Tabs to spaces

--- src/fft.js
+++ src/fft.js
@@ -9,16 +9,16 @@ function generateFFTTableShort(len) {
     }
     
     f[0][0] = 1;
-	f[0][1] = 0;
-	var lastImag = 0;
-	
-	for (var i = 1; i < len; i++) {
-	    f[i][0] = f[i - 1][0] * cosT + lastImag * sinT;
-		lastImag = lastImag * cosT - f[i-1][0] * sinT;
-		f[i][1] = -lastImag;
-	}
-	
-	return f;
+    f[0][1] = 0;
+    var lastImag = 0;
+    
+    for (var i = 1; i < len; i++) {
+        f[i][0] = f[i - 1][0] * cosT + lastImag * sinT;
+        lastImag = lastImag * cosT - f[i-1][0] * sinT;
+        f[i][1] = -lastImag;
+    }
+    
+    return f;
 }
 
 function generateFFTTableLong(len) {
@@ -32,16 +32,16 @@ function generateFFTTableLong(len) {
     }
     
     f[0][0] = 1;
-	f[0][1] = 0;
-	f[0][2] = 0;
-	
-	for (var i = 1; i < len; i++) {
-	    f[i][0] = f[i - 1][0] * cosT + f[i - 1][2] * sinT;
-		f[i][2] = f[i - 1][2] * cosT - f[i - 1][0] * sinT;
-		f[i][1] = -f[i][2];
-	}
-	
-	return f;
+    f[0][1] = 0;
+    f[0][2] = 0;
+    
+    for (var i = 1; i < len; i++) {
+        f[i][0] = f[i - 1][0] * cosT + f[i - 1][2] * sinT;
+        f[i][2] = f[i - 1][2] * cosT - f[i - 1][0] * sinT;
+        f[i][1] = -f[i][2];
+    }
+    
+    return f;
 }
 
 function FFT(length) {
@@ -110,56 +110,56 @@ FFT.prototype.process = function(input, forward) {
     }
     
     // bottom base-4 round
-	for(var i = 0; i<length; i += 4) {
-		a[0] = input[i][0] + input[i+1][0];
-		a[1] = input[i][1] + input[i+1][1];
-		b[0] = input[i + 2][0] + input[i+3][0];
-		b[1] = input[i + 2][1] + input[i+3][1];
-		c[0] = input[i][0] - input[i + 1][0];
-		c[1] = input[i][1] - input[i + 1][1];
-		d[0] = input[i + 2][0] - input[i + 3][0];
-		d[1] = input[i + 2][1] - input[i + 3][1];
-		input[i][0] = a[0] + b[0];
-		input[i][1] = a[1] + b[1];
-		input[i + 2][0] = a[0] - b[0];
-		input[i + 2][1] = a[1] - b[1];
+    for(var i = 0; i<length; i += 4) {
+        a[0] = input[i][0] + input[i+1][0];
+        a[1] = input[i][1] + input[i+1][1];
+        b[0] = input[i + 2][0] + input[i+3][0];
+        b[1] = input[i + 2][1] + input[i+3][1];
+        c[0] = input[i][0] - input[i + 1][0];
+        c[1] = input[i][1] - input[i + 1][1];
+        d[0] = input[i + 2][0] - input[i + 3][0];
+        d[1] = input[i + 2][1] - input[i + 3][1];
+        input[i][0] = a[0] + b[0];
+        input[i][1] = a[1] + b[1];
+        input[i + 2][0] = a[0] - b[0];
+        input[i + 2][1] = a[1] - b[1];
 
-		e1[0] = c[0] - d[1];
-		e1[1] = c[1] + d[0];
-		e2[0] = c[0] + d[1];
-		e2[1] = c[1] - d[0];
-		
-		if(forward) {
-			input[i + 1][0] = e2[0];
-			input[i + 1][1] = e2[1];
-			input[i + 3][0] = e1[0];
-			input[i + 3][1] = e1[1];
-		} else {
-			input[i + 1][0] = e1[0];
-			input[i + 1][1] = e1[1];
-			input[i + 3][0] = e2[0];
-			input[i + 3][1] = e2[1];
-		}
-	}
-	
-	// iterations from bottom to top
-	for(var i = 4; i < length; i <<= 1) {
-		var shift = i << 1,
-		    m = length / shift;
-		    
-		for(var j = 0; j < length; j += shift) {
-			for(var k = 0; k < i; k++) {
-				var km = k * m,
-				    rootRe = roots[km][0],
-				    rootIm = roots[km][imOffset],
-				    zRe = input[i + j + k][0] * rootRe - input[i + j + k][1] * rootIm,
-				    zIm = input[i + j + k][0] * rootIm + input[i + j + k][1] * rootRe;
+        e1[0] = c[0] - d[1];
+        e1[1] = c[1] + d[0];
+        e2[0] = c[0] + d[1];
+        e2[1] = c[1] - d[0];
+        
+        if(forward) {
+            input[i + 1][0] = e2[0];
+            input[i + 1][1] = e2[1];
+            input[i + 3][0] = e1[0];
+            input[i + 3][1] = e1[1];
+        } else {
+            input[i + 1][0] = e1[0];
+            input[i + 1][1] = e1[1];
+            input[i + 3][0] = e2[0];
+            input[i + 3][1] = e2[1];
+        }
+    }
+    
+    // iterations from bottom to top
+    for(var i = 4; i < length; i <<= 1) {
+        var shift = i << 1,
+            m = length / shift;
+            
+        for(var j = 0; j < length; j += shift) {
+            for(var k = 0; k < i; k++) {
+                var km = k * m,
+                    rootRe = roots[km][0],
+                    rootIm = roots[km][imOffset],
+                    zRe = input[i + j + k][0] * rootRe - input[i + j + k][1] * rootIm,
+                    zIm = input[i + j + k][0] * rootIm + input[i + j + k][1] * rootRe;
 
-				input[i + j + k][0] = (input[j + k][0] - zRe) * scale;
-				input[i + j + k][1] = (input[j + k][1] - zIm) * scale;
-				input[j + k][0] = (input[j + k][0] + zRe) * scale;
-				input[j + k][1] = (input[j + k][1] + zIm) * scale;
-			}
-		}
-	}
+                input[i + j + k][0] = (input[j + k][0] - zRe) * scale;
+                input[i + j + k][1] = (input[j + k][1] - zIm) * scale;
+                input[j + k][0] = (input[j + k][0] + zRe) * scale;
+                input[j + k][1] = (input[j + k][1] + zIm) * scale;
+            }
+        }
+    }
 }
