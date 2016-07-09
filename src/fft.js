c355a7654d00683ff865c52edaaa8da4384389a0
Devon Govett <devongovett@gmail.com>
2016-07-09 11:11:43 -0700

    Simplify and speed up FFT
    * No more long vs short tables.
    * Precompute bit reversal table.

--- src/fft.js
+++ src/fft.js
@@ -20,74 +20,33 @@
     
 function FFT(length) {
     this.length = length;
-
-    switch (length) {
-        case 64:
-            this.roots = generateFFTTableShort(64);
-            break;
-
-        case 512:
-            this.roots = generateFFTTableLong(512);
-            break;
-
-        case 60:
-            this.roots = generateFFTTableShort(60);
-            break;
-
-        case 480:
-            this.roots = generateFFTTableLong(480);
-            break;
-
-        default:
-            throw new Error("unexpected FFT length: " + length);
-    }
+    
+    this.roots = generateFFTTable(length);
 
     // processing buffers
-    this.rev = new Array(length);
-    for (var i = 0; i < length; i++) {
-        this.rev[i] = new Float32Array(2);
-    }
+    this.buf = new Float32Array(length * 2);
+    
+    // Bit reversal lookup table
+    this.rev = new Uint16Array(length);
+    var ii = 0;
+    for (let i = 0; i < length; i++) {
+      this.rev[ii] = i;
+      
+      let k = length >>> 1;
+      while (ii >= k && k > 0) {
+          ii -= k;
+          k >>= 1;
+      }
 
-    this.a = new Float32Array(2);
-    this.b = new Float32Array(2);
-    this.c = new Float32Array(2);
-    this.d = new Float32Array(2);     
-    this.e1 = new Float32Array(2);
-    this.e2 = new Float32Array(2);
+      ii += k;
+    }
 }
 
-function generateFFTTableShort(len) {
+function generateFFTTable(len) {
     var t = 2 * Math.PI / len,
         cosT = Math.cos(t),
         sinT = Math.sin(t),
-        f = new Array(len);
-
-    for (var i = 0; i < len; i++) {
-        f[i] = new Float32Array(2);
-    }
-
-    f[0][0] = 1;
-    f[0][1] = 0;
-    var lastImag = 0;
-
-    for (var i = 1; i < len; i++) {
-        f[i][0] = f[i - 1][0] * cosT + lastImag * sinT;
-        lastImag = lastImag * cosT - f[i - 1][0] * sinT;
-        f[i][1] = -lastImag;
-    }
-
-    return f;
-}
-
-function generateFFTTableLong(len) {
-    var t = 2 * Math.PI / len,
-        cosT = Math.cos(t),
-        sinT = Math.sin(t),
-        f = new Array(len);
-
-    for (var i = 0; i < len; i++) {
-        f[i] = new Float32Array(3);
-    }
+        f[len][3] = new Float32Array(len * 3);
 
     f[0][0] = 1;
     f[0][1] = 0;
@@ -102,70 +61,55 @@ function generateFFTTableLong(len) {
     return f;
 }
 
-FFT.prototype.process = function(input, forward) {
+FFT.prototype.process = function(input[length][2], forward) {
     var length = this.length,
         imOffset = (forward ? 2 : 1),
         scale = (forward ? length : 1),
-        rev = this.rev,
-        roots = this.roots;
+        buf[length][2] = this.buf,
+        roots[length][3] = this.roots,
+        rev = this.rev;
 
     // bit-reversal
-    var ii = 0;
     for (var i = 0; i < length; i++) {
-        rev[i][0] = input[ii][0];
-        rev[i][1] = input[ii][1];
-
-        var k = length >>> 1;
-        while (ii >= k && k > 0) {
-            ii -= k;
-            k >>= 1;
-        }
-
-        ii += k;
+        buf[i][0] = input[rev[i]][0];
+        buf[i][1] = input[rev[i]][1];
     }
 
-    var a = this.a,
-        b = this.b,
-        c = this.c,
-        d = this.d,
-        e1 = this.e1,
-        e2 = this.e2;
-
     for (var i = 0; i < length; i++) {
-        input[i][0] = rev[i][0];
-        input[i][1] = rev[i][1];
+        input[i][0] = buf[i][0];
+        input[i][1] = buf[i][1];
     }
 
     // bottom base-4 round
     for (var i = 0; i < length; i += 4) {
-        a[0] = input[i][0] + input[i + 1][0];
-        a[1] = input[i][1] + input[i + 1][1];
-        b[0] = input[i + 2][0] + input[i + 3][0];
-        b[1] = input[i + 2][1] + input[i + 3][1];
-        c[0] = input[i][0] - input[i + 1][0];
-        c[1] = input[i][1] - input[i + 1][1];
-        d[0] = input[i + 2][0] - input[i + 3][0];
-        d[1] = input[i + 2][1] - input[i + 3][1];
-        input[i][0] = a[0] + b[0];
-        input[i][1] = a[1] + b[1];
-        input[i + 2][0] = a[0] - b[0];
-        input[i + 2][1] = a[1] - b[1];
+        let a0 = input[i][0] + input[i + 1][0];
+        let a1 = input[i][1] + input[i + 1][1];
+        let b0 = input[i + 2][0] + input[i + 3][0];
+        let b1 = input[i + 2][1] + input[i + 3][1];
+        let c0 = input[i][0] - input[i + 1][0];
+        let c1 = input[i][1] - input[i + 1][1];
+        let d0 = input[i + 2][0] - input[i + 3][0];
+        let d1 = input[i + 2][1] - input[i + 3][1];
+        input[i][0] = a0 + b0;
+        input[i][1] = a1 + b1;
+        input[i + 2][0] = a0 - b0;
+        input[i + 2][1] = a1 - b1;
 
-        e1[0] = c[0] - d[1];
-        e1[1] = c[1] + d[0];
-        e2[0] = c[0] + d[1];
-        e2[1] = c[1] - d[0];
+        let e10 = c0 - d1;
+        let e11 = c1 + d0;
+        let e20 = c0 + d1;
+        let e21 = c1 - d0;
 
         if (forward) {
-            input[i + 1][0] = e2[0];
-            input[i + 1][1] = e2[1];
-            input[i + 3][0] = e1[0];
-            input[i + 3][1] = e1[1];
+            input[i + 1][0] = e20;
+            input[i + 1][1] = e21;
+            input[i + 3][0] = e10;
+            input[i + 3][1] = e11;
         } else {
-            input[i + 1][0] = e1[0];
-            input[i + 1][1] = e1[1];
-            input[i + 3][0] = e2[0];
-            input[i + 3][1] = e2[1];
+            input[i + 1][0] = e10;
+            input[i + 1][1] = e11;
+            input[i + 3][0] = e20;
+            input[i + 3][1] = e21;
         }
     }
 
